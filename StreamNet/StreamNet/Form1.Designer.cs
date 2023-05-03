ed29af4e6682e860339f76e3c0aed6ec58c7dd07
Karina <105536477+KarinaShupik@users.noreply.github.com>
2023-05-03 11:48:46 +0300

    Thread

---
+++ StreamNet/StreamNet/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace StreamNet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(32, 36);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(220, 129);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 301);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(220, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Запустити 1 потік";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(32, 336);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(220, 28);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Зупинити 1 потік";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(280, 301);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(226, 29);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Запустити 2 потік";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(280, 337);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(226, 27);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Зупинити 2 потік";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(541, 301);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(215, 29);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Запустити 3 потік";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(541, 337);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(215, 27);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Зупинити 3 потік";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(32, 383);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(724, 27);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Запустити всі потоки";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(32, 417);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(724, 28);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "Зупинити всі потоки";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(280, 36);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(226, 129);
+            this.richTextBox2.TabIndex = 11;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(541, 36);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(215, 129);
+            this.richTextBox3.TabIndex = 12;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(32, 172);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(220, 96);
+            this.richTextBox4.TabIndex = 13;
+            this.richTextBox4.Text = "";
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.Location = new System.Drawing.Point(280, 172);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.Size = new System.Drawing.Size(226, 96);
+            this.richTextBox5.TabIndex = 14;
+            this.richTextBox5.Text = "";
+            // 
+            // richTextBox6
+            // 
+            this.richTextBox6.Location = new System.Drawing.Point(541, 172);
+            this.richTextBox6.Name = "richTextBox6";
+            this.richTextBox6.Size = new System.Drawing.Size(215, 96);
+            this.richTextBox6.TabIndex = 15;
+            this.richTextBox6.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 470);
+            this.Controls.Add(this.richTextBox6);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.RichTextBox richTextBox6;
+    }
+}
+
---
+++ StreamNet/StreamNet/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Security.Cryptography;
+
+using Org.BouncyCastle.Crypto.Digests;
+
+using System.IO;
+using Org.BouncyCastle.Crypto;
+
+namespace StreamNet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            thread1 = new Thread(new ThreadStart(des));
+            thread2 = new Thread(new ThreadStart(snefru));
+            thread3 = new Thread(new ThreadStart(backpack));
+        }
+
+        Thread thread1;
+        Thread thread2;
+        Thread thread3;
+
+        private void des()
+        {
+            try
+            {
+                // Get the plaintext and key from the input boxes
+                string plaintext = richTextBox1.Text;
+                string key = "01234567";
+
+                // Convert the plaintext and key to byte arrays
+                byte[] plaintextBytes = Encoding.UTF8.GetBytes(plaintext);
+                byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+
+                // Create a new instance of the DES algorithm with the key
+                DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+                des.Key = keyBytes;
+
+                // Create a new MemoryStream to hold the encrypted data
+                MemoryStream ms = new MemoryStream();
+
+                // Create a new CryptoStream to encrypt the data
+                CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write);
+
+                // Write the plaintext to the CryptoStream
+                cs.Write(plaintextBytes, 0, plaintextBytes.Length);
+
+                // Flush the final block of data through the CryptoStream
+                cs.FlushFinalBlock();
+
+                // Convert the encrypted data to a Base64 string and output it to the result box
+                string encryptedString = Convert.ToBase64String(ms.ToArray());
+
+                // Output the encrypted text to RichTextBox2 (on the UI thread)
+                Invoke(new Action(() => richTextBox4.Text = encryptedString));
+            }
+            catch (ThreadAbortException)
+            {
+                // Thread was aborted, do nothing
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        
+        private void snefru()
+        {
+            try
+            {
+                // Get the plaintext from the input box
+                string plaintext = richTextBox2.Text;
+
+                // Convert the plaintext to a byte array
+                byte[] plaintextBytes = Encoding.UTF8.GetBytes(plaintext);
+
+                // Create a new SHA-256 digest
+                SHA256 sha256 = SHA256.Create();
+
+                // Compute the first digest
+                byte[] digest1 = sha256.ComputeHash(plaintextBytes);
+
+                // Compute the second digest by hashing the first digest
+                byte[] digest2 = sha256.ComputeHash(digest1);
+
+                // Concatenate the two digests
+                byte[] combinedDigest = new byte[digest1.Length + digest2.Length];
+                Buffer.BlockCopy(digest1, 0, combinedDigest, 0, digest1.Length);
+                Buffer.BlockCopy(digest2, 0, combinedDigest, digest1.Length, digest2.Length);
+
+                // Compute the final digest by hashing the concatenated digest
+                byte[] finalDigest = sha256.ComputeHash(combinedDigest);
+
+                // Convert the final digest to a Base64 string and output it to the result box
+                string encryptedString = Convert.ToBase64String(finalDigest);
+
+                // Output the encrypted text to RichTextBox3 (on the UI thread)
+                Invoke(new Action(() => richTextBox5.Text = encryptedString));
+            }
+            catch (ThreadAbortException)
+            {
+                // Thread was aborted, do nothing
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void backpack()
+        {
+
+            try
+            {
+                // Get the plaintext from the input box
+                string plaintext = richTextBox3.Text;
+
+                // Define the public and private keys
+                int[] publicKey = { 2, 3, 7, 14, 30, 57, 120, 251 };
+                int privateKey = 23;
+
+                // Convert the plaintext to binary
+                string binaryString = string.Join("", Encoding.ASCII.GetBytes(plaintext).Select(b => Convert.ToString(b, 2).PadLeft(8, '0')));
+
+                // Ensure that binaryString has the same length as publicKey
+                if (binaryString.Length < publicKey.Length)
+                {
+                    binaryString = binaryString.PadRight(publicKey.Length, '0');
+                }
+
+                // Calculate the sum of the binary string multiplied by the public key
+                long sum = 0;
+                for (int i = 0; i < publicKey.Length; i++)
+                {
+                    if (binaryString[i] == '1')
+                    {
+                        sum += publicKey[i];
+                    }
+                };
+
+
+                // Encrypt the sum using the private key
+                long encryptedSum = (sum * privateKey) % 499;
+
+                // Output the encrypted text to RichTextBox4 (on the UI thread)
+                Invoke(new Action(() => richTextBox6.Text = encryptedSum.ToString()));
+            }
+            catch (ThreadAbortException)
+            {
+                // Thread was aborted, do nothing
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            thread1.Start();
+            //draw_rect();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            thread2.Start();
+            //draw_eclips();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            thread3.Start();
+            //Rnd_num();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            thread1.Suspend();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            thread2.Suspend();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            thread3.Suspend();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+            //draw_eclips();
+            //draw_rect();
+            //Rnd_num();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            thread1.Suspend();
+            thread2.Suspend();
+            thread3.Suspend();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            thread1.Abort();
+            thread2.Abort();
+            thread3.Abort();
+        }
+
+        
+    }
+
+    
+}
+
